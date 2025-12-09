d9db6d291fb5a66e9f8a0a2384b90ecb9b66bc02
GitHub User <user@example.com>
2025-12-09 18:51:59 +0800

    修复WelcomeActivity中缺少MotionEvent导入的问题

--- app/src/main/java/com/example/testapp/WelcomeActivity.java
+++ app/src/main/java/com/example/testapp/WelcomeActivity.java
@@ -2,6 +2,7 @@ package com.example.testapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
